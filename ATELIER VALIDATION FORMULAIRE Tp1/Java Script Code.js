15ede84d79ba41ff31a09327d1482616a17952de
Hamza Mazine <62185746+MrMazine@users.noreply.github.com>
2022-03-30 08:41:43 +0000

    Update Java Script Code.js

--- ATELIER VALIDATION FORMULAIRE Tp1/Java Script Code.js
+++ ATELIER VALIDATION FORMULAIRE Tp1/Java Script Code.js
@@ -2,28 +2,23 @@ function afficher(){
 		 var check1 = document.getElementById('chkYes');
 		 var check2 = document.getElementById('chkNo');
 		 var divdisp = document.getElementById('Div1');
-
-
 		 if (check1.checked) {
-			 	divdisp.style.display = "block";
-			 	
+			 divdisp.style.display = "block";	
 		 }
 		 if (check2.checked){
-		 		divdisp.style.display = "none";
+		 	divdisp.style.display = "none";
 		 } 
  }
   function validate(){
+ 	var rg = new RegExp('^[a-z A-Z]+$','g');
+ 	var rgPass = new RegExp('^[A-Za-z0-9]{8,20}$','g');
+	var validPass = document.getElementById('Password').value;
+ 	var validname = document.getElementById('userName').value;
 
- 		var rg = new RegExp('^[a-z A-Z]+$','g');
- 		var rgPass = new RegExp('^[A-Za-z0-9]{8,20}$','g');
-		var validPass = document.getElementById('Password').value;
- 		var validname = document.getElementById('userName').value;
-
- 		if (rg.test(validname) == false){
- 			alert('incorrect name');
- 		}
- 		if ((rgPass.test(validPass)) == false){
- 			alert('incorrect Password');
- 		}
-
+ 	if (rg.test(validname) == false){
+ 		alert('incorrect name');
+ 	}
+ 	if ((rgPass.test(validPass)) == false){
+ 		alert('incorrect Password');
+ 	}
  }
